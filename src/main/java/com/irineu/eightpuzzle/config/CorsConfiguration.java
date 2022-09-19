cf55bfb09ccf87ac5966c2896ae30486a5cbfc5c
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-09-19 01:26:28 -0300

    Update CorsConfiguration.java

--- src/main/java/com/irineu/eightpuzzle/config/CorsConfiguration.java
+++ src/main/java/com/irineu/eightpuzzle/config/CorsConfiguration.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://127.0.0.1:5173/") // Endereço do FrontEnd
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://8-puzzle-front.vercel.app") // Endereço do FrontEnd
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
